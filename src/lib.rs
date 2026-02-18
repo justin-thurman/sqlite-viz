c3b34fadf10ee33e977a066171a6e78bd964bd52
Justin Thurman <thurman.justin@gmail.com>
2026-02-18 10:07:43 -0500

    Parse page size from database header

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-mod utils;
 mod model;
 mod parser;
+mod utils;
 
+use crate::parser::parse_database;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -17,5 +18,10 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn analyze_db(bytes: &[u8]) -> Result<JsValue, JsValue> {
-    Ok(JsValue::from(bytes.len()))
+    match parse_database(bytes) {
+        Ok(result) => Ok(JsValue::from(result.database_header.page_size)),
+        Err(_) => {
+            panic!("Fix me")
+        }
+    }
 }
