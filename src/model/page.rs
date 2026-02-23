d4989287af73c0fe7314d4fddc374983924ed5ff
Justin Thurman <thurman.justin@gmail.com>
2026-02-23 08:52:34 -0500

    Parse page header

--- src/model/page.rs
+++ src/model/page.rs
@@ -1,5 +1,8 @@
 use crate::model::database::DatabaseHeader;
+use crate::model::page::BTreePageType::{IndexInterior, IndexLeaf, TableInterior, TableLeaf};
 use serde::Serialize;
+use std::convert::TryFrom;
+use thiserror::Error;
 
 #[derive(Debug, Serialize, Eq, PartialEq)]
 pub struct Page {}
@@ -7,4 +10,49 @@ pub struct Page {}
 #[derive(Debug, Serialize, Eq, PartialEq)]
 pub struct FirstPage {
     pub database_header: DatabaseHeader,
+    pub page_header: PageHeader,
+}
+
+#[derive(Debug, Serialize, Eq, PartialEq)]
+pub struct PageHeader {
+    pub page_type: BTreePageType, // TODO: currently treating all pages as B-tree; will decide how to split up later
+    pub first_freeblock_start: u16, // 0 -> no freeblocks
+    pub num_cells: u16,
+    pub cell_content_area_start: u16, // 0 -> 65536
+    pub num_fragmented_free_bytes: u8,
+    pub rightmost_ptr: Option<u32>, // present only for interior b-tree pages
+}
+
+#[derive(Debug, Serialize, Eq, PartialEq)]
+pub enum BTreePageType {
+    IndexInterior,
+    IndexLeaf,
+    TableInterior,
+    TableLeaf,
+}
+
+impl BTreePageType {
+    pub fn is_interior(&self) -> bool {
+        self == &TableInterior || self == &IndexInterior
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum BTreePageTypeError {
+    #[error("Invalid B-tree page type: {0}")]
+    InvalidPageType(u8),
+}
+
+impl TryFrom<u8> for BTreePageType {
+    type Error = BTreePageTypeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            2 => Ok(IndexInterior),
+            5 => Ok(TableInterior),
+            10 => Ok(IndexLeaf),
+            13 => Ok(TableLeaf),
+            _ => Err(BTreePageTypeError::InvalidPageType(value)),
+        }
+    }
 }
